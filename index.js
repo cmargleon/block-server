752e4e4bc7fee5639726ff415508f26df621b6b9
cmargleon <cmargleon@hotmail.com>
2018-08-25 00:30:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var path = require('path');
+//var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//var mongoose = require('mongoose');
+
+var appRoutes = require('./routes/routes');
+
+var app = express();
+//mongoose.connect('localhost:27017/node-angular');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/', appRoutes);
+
+// catch 404 and forward to error handler
+/*
+app.use(function (req, res, next) {
+    return res.render('index');
+});
+*/
+
+//declare port
+var port = process.env.PORT || 8000;
+if (process.env.VCAP_APPLICATION) {
+ port = process.env.PORT;
+}
+
+//run app on port
+app.listen(port, function() {
+ console.log('app running on port: %d', port);
+});
