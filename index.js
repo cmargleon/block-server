42de5b35f3087740a5febfbc66396cbb147115ba
claudio <cmargleon@gmail.com>
2018-08-27 17:00:11 -0300

    user route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var appRoutes = require('./routes/routes');
+var usersRoute = require('./routes/user');
 
 var app = express();
 mongoose.connect('localhost:27017/myCollection');
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.use('/route', appRoutes);
+app.use('/user', usersRoute);
 
 // catch 404 and forward to error handler
 /*
