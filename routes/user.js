7bb47c7519630fd86f04e39998d78ca774422f5e
claudio <cmargleon@gmail.com>
2018-08-30 18:47:25 -0300

    user sign up

--- routes/user.js
+++ routes/user.js
@@ -17,32 +17,41 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/signup', async function(req, res, next) {
-    console.log(req.body)
-    bcrypt.hash(req.body.password, 10, (err, hash)=> {
-        if(err) {
-            return res.status(500).json({
-                error: err
-            });
-        } else {
-            const user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                email: req.body.email,
-                password: hash
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if(user) {
+                return res.status(409).json({
+                    message: "Este e-mail ya se encuentra registrado"
                 });
-                user
-                .save()
-                .then(result => {
-                    res.status(201).json({
-                        message: "User Created"
-                    });
-                })
-                .catch(err => {
-                    res.status(500).json({
-                        error: err
-                    });
+            } else {
+                bcrypt.hash(req.body.password, 10, (err, hash)=> {
+                    if(err) {
+                        return res.status(500).json({
+                            error: err
+                        });
+                    } else {
+                        const user = new User({
+                            _id: new mongoose.Types.ObjectId(),
+                            email: req.body.email,
+                            password: hash
+                            });
+                            user
+                            .save()
+                            .then(result => {
+                                res.status(201).json({
+                                    message: "User Created"
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
                 });
-        }
-    });
+            }
+        })
 });
 
 module.exports = router;
