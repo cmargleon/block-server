b0836a2172548052f4b5c5a93256739b9c49ae7f
claudio <cmargleon@gmail.com>
2018-09-03 18:05:44 -0300

    composer registry university

--- routes/university.js
+++ routes/university.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const universityController = require('../controllers/university');
-
+const userComposer = require('../composer-functions/university');
 
 router.get('/', function (req, res, next) {
     res.send("ok")
+});
+
+router.post('/', function (req, res, next) {
+    userComposer(req.body.cardId, req.body.universityRut, req.body.shortName, req.body.fullName, req.body.email)
+    .then(result => {
+        res.status(200).json({
+            message: "Usuario creado en la Blockchain"
+        });
+    })
+    .catch()
 });
 
 router.post('/signup', universityController.university_signup);
