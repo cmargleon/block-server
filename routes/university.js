e233ad861086a6371ab215ba9267fd3142449754
claudio <cmargleon@gmail.com>
2018-09-04 17:24:02 -0300

    fixing

--- routes/university.js
+++ routes/university.js
@@ -3,55 +3,23 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const universityController = require('../controllers/university');
 const userComposer = require('../composer-functions/university');
-const universityComposer = require('./university2');
-const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
-const AdminConnection = require('composer-admin').AdminConnection;
-const log4js = require('log4js');
 
 router.get('/', function (req, res, next) {
     res.send("ok")
 });
 
-router.post('/', async function (req, res, next) {
-    logger.info('Empezando función');
-    try {
-        logger.info('Dentro try');
-        //connect as admin
-        businessNetworkConnection = new BusinessNetworkConnection();
-        await businessNetworkConnection.connect('admin@degree');
-        logger.info('Dp de business connection');
-        //get the factory for the business network
-        factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-        logger.info('dp de factory');
-        //create university participant
-        const university = factory.newResource(namespace, 'University', universityRut);
-        university.shortName = shortName;
-        university.fullName = fullName;
-        university.email = email;
-        logger.info('dp de new resource');
-        //add university participant
-        const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.University');
-        await participantRegistry.add(university);
-        logger.info('dp de add participant');
-        //issue identity
-        const identity = await businessNetworkConnection.issueIdentity(namespace + '.University#' + universityRut, cardId);
-        logger.info('dp de issue identity');
-        //import card for identity
-        await importCardForIdentity(cardId, identity);
-        logger.info('dp de import card');
-        //disconnect
-        await businessNetworkConnection.disconnect('admin@degree');
-     
-        return res.send(200).json({
-            message: "success"
+router.post('/', function (req, res, next) {
+    userComposer(req.body.cardId, req.body.universityRut, req.body.shortName, req.body.fullName, req.body.email)
+    .then(result => {
+        return res.status(200).json({
+            message: "Usuario creado en la Blockchain"
         });
-     
-       } catch (err) {
-        res.status(500).json({
-            message: "something went wrong",
-            error: err
+    })
+    .catch(err => {
+        return res.status(500).json({
+            message: "Algo fue mal"
         })
-       }
+    })
 });
 
 router.post('/signup', universityController.university_signup);
