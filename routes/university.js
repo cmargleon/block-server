bd95f50b3c0496cb16822e3144bd70483c3652c1
claudio <cmargleon@gmail.com>
2018-09-04 17:19:52 -0300

    fixing

--- routes/university.js
+++ routes/university.js
@@ -6,37 +6,39 @@ const userComposer = require('../composer-functions/university');
 const universityComposer = require('./university2');
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 const AdminConnection = require('composer-admin').AdminConnection;
+const log4js = require('log4js');
 
 router.get('/', function (req, res, next) {
     res.send("ok")
 });
 
 router.post('/', async function (req, res, next) {
+    logger.info('Empezando función');
     try {
-  
+        logger.info('Dentro try');
         //connect as admin
         businessNetworkConnection = new BusinessNetworkConnection();
         await businessNetworkConnection.connect('admin@degree');
-     
+        logger.info('Dp de business connection');
         //get the factory for the business network
         factory = businessNetworkConnection.getBusinessNetwork().getFactory();
-     
+        logger.info('dp de factory');
         //create university participant
         const university = factory.newResource(namespace, 'University', universityRut);
         university.shortName = shortName;
         university.fullName = fullName;
         university.email = email;
-     
+        logger.info('dp de new resource');
         //add university participant
         const participantRegistry = await businessNetworkConnection.getParticipantRegistry(namespace + '.University');
         await participantRegistry.add(university);
-     
+        logger.info('dp de add participant');
         //issue identity
         const identity = await businessNetworkConnection.issueIdentity(namespace + '.University#' + universityRut, cardId);
-     
+        logger.info('dp de issue identity');
         //import card for identity
         await importCardForIdentity(cardId, identity);
-     
+        logger.info('dp de import card');
         //disconnect
         await businessNetworkConnection.disconnect('admin@degree');
      
